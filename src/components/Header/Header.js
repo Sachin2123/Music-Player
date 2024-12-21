8244d7d972352e404c8752ced0bb68fc4fc2c6ab
Sachin2123 <sachinvishwakarma8286@gmail.com>
2024-12-21 16:28:30 +0530

    player modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search } from "lucide-react";
 import "./Header.scss";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+  const HandleChange = (e) => {
+    // console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+
   return (
     <div style={{ backgroundColor: "#121212" }}>
       <div className="title">For You</div>
       <div className="header">
         <div className="input-container">
-          <input type="text" placeholder="Search Song, Artist" />
+          <input
+            value={search}
+            onChange={HandleChange}
+            type="text"
+            placeholder="Search Song, Artist"
+          />
           <Search className="search-icon" />
         </div>
       </div>
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,15 +1,23 @@
-import React from "react";
-import {
-  FastForwardIcon,
-  FastRewindIcon,
-  PlayCircleRoundedIcon,
-  PauseCircleOutlineRoundedIcon,
-  FavoriteBorderRoundedIcon,
-} from "@mui/icons-material";
-
+import React, { useState } from "react";
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Button from "@mui/material/Button";
+import FastForwardIcon from "@mui/icons-material/FastForward";
+import FastRewindIcon from "@mui/icons-material/FastRewind";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import { Volume2, CircleEllipsis } from "lucide-react";
+import Slider from "@mui/material/Slider";
 import "./Player.scss";
 
 const Player = ({ currentSong }) => {
+  const [click, setClick] = useState(PlayCircleIcon);
+  const [position, setPosition] = useState(false);
+
+  const handlePlayPause = (event) => {
+    console.log("clicked");
+    setClick(!click);
+  };
   return (
     <div className="player">
       {currentSong ? (
@@ -25,6 +33,74 @@ const Player = ({ currentSong }) => {
               src={currentSong.cover}
               alt={currentSong.title}
             />
+            <Slider
+              aria-label="time-indicator"
+              size="small"
+              // value={position}
+              min={0}
+              step={1}
+              // max={duration}
+              onChange={(_, value) => setPosition(value)}
+              sx={(t) => ({
+                marginTop: "10px",
+                color: "white",
+                height: 4,
+                "& .MuiSlider-thumb": {
+                  width: 8,
+                  height: 8,
+                  transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+                  "&::before": {
+                    boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+                  },
+                  "&:hover, &.Mui-focusVisible": {
+                    boxShadow: `0px 0px 0px 8px ${"rgb(0 0 0 / 16%)"}`,
+                    ...t.applyStyles("dark", {
+                      boxShadow: `0px 0px 0px 8px ${"rgb(255 255 255 / 16%)"}`,
+                    }),
+                  },
+                  "&.Mui-active": {
+                    width: 20,
+                    height: 20,
+                  },
+                },
+                "& .MuiSlider-rail": {
+                  opacity: 0.28,
+                },
+                ...t.applyStyles("dark", {
+                  color: "#fff",
+                }),
+              })}
+            />
+            <div className="controls">
+              <Button>
+                <CircleEllipsis color="white" backgroundColor="#36454F" />
+              </Button>
+              <div>
+                <Button>
+                  <FastRewindIcon sx={{ color: "grey", fontSize: "30px" }} />
+                </Button>
+
+                {!click ? (
+                  <Button onClick={handlePlayPause}>
+                    <PlayCircleIcon sx={{ color: "white", fontSize: "40px" }} />
+                  </Button>
+                ) : (
+                  <Button onClick={handlePlayPause}>
+                    <PauseCircleIcon
+                      sx={{ color: "white", fontSize: "40px" }}
+                    />
+                  </Button>
+                )}
+
+                <Button>
+                  <FastForwardIcon sx={{ color: "grey", fontSize: "30px" }} />
+                </Button>
+              </div>
+
+              <Button>
+                <Volume2 color="white" backgroundColor="#36454F" />
+              </Button>
+            </div>
           </div>
         </>
       ) : (
